c8fbfa7dfa98dc7f9d65f70be8eeb771b8c694f4
Corentin Turcaud <corentin.turcaud@gmail.com>
2017-10-19 14:31:53 +0200

    Update FonctionEvaluation (again)

--- FonctionEvaluation.py
+++ FonctionEvaluation.py
@@ -1,50 +1,41 @@
 POINTSADJACENTS = 5
 POINTSD2 = 10
 POINTTRIANGLE = 15
-POINTSBLOCK = 15
-
-tableau = [] #plateau de jeu
-joueur = 1 #id du joueur
-for i in range(11):
-    tableau.append([0] * 11)
-
-coupsDispo = [] #tableau des cases vides
-tableauValue = [] #tableau indiquand les valeurs pour chaque coup
-
-for x,y in coupsDispo:
-    value = 0
-    nbAdjacents = nbAdjacentsAlly(x,y)
-    value +=  nbAdjacents * POINTSADJACENTS
-    if nbAdjacents == 3 and triangle(x,y):
-        value += POINTTRIANGLE
-    tableauValue.append((x,y,value))
+POINTSOPPOSITE = 15
 
 def fonctionEvaluation(x,y,plateau,joueur):
-    #todo
-    pass
+    value = 0
 
-def nbAdjacentsAlly(x,y):
+    nbAdjacentsAlly = nbAdjacents(x,y,plateau,joueur)
+    value += nbAdjacentsAlly * 5
+    if nbAdjacentsAlly == 3 and triangle(x,y,plateau,joueur):
+        value += 10
+    if nbAdjacentsAlly == 2 and opposite(x,y,plateau,joueur):
+        value += 10
+
+    nbAdjacentsEnemy = nbAdjacents(x,y,tableau,-joueur)
+    if nbAdjacentsEnemy == 2 and opposite(x,y,plateau,-joueur):
+        value += 15
+    if nbAdjacentsEnemy == 3 and opposite(x, y , plateau , -joueur):
+        value += 15
+
+    return value
+
+def nbAdjacents(x,y,tableau,joueur):
     nb = 0
     for i in range(x-1,x+1):
         for j in range(y-1,y+1):
             if j!=i and tableau[i][j] and isLegal(i,j) == joueur: nb+=1
     return nb
 
-def nbAdjacentsEnemy(x,y):
-    nb = 0
-    for i in range(x-1,x+1):
-        for j in range(y-1,y+1):
-            if j!=i and tableau[i][j] and isLegal(i,j) == -joueur: nb+=1
-    return nb
-
-def triangle(x,y):
+def triangle(x,y,tableau,joueur):
     if x in range(1,10) and y in range(1,10):
         if (tableau[x-1][y+1] == joueur and tableau[x][y-1] == joueur and tableau[x+1][y] == joueur)\
         or (tableau[x-1][y] == joueur and tableau[x][y+1] == joueur and tableau[x+1][y-1] == joueur):
             return true
     else : return false
 
-def opposite(x,y):
+def opposite(x,y,tableau,joueur):
     if x in  range(1,10) and y in range(1,10):
         if (tableau[x][y-1] == joueur and tableau[x][y+1] == joueur)\
         or (tableau[x-1][y] == joueur and tableau[x+1][y] == joueur) \
